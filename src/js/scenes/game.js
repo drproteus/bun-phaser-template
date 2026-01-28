4434c3b595f2a3d5ef97e60c28d229c3e5eff271
Jake Goritski <jgoritski@gmail.com>
2026-01-28 09:14:46 -0800

    render playfield as tilemap and update

--- src/js/scenes/game.js
+++ src/js/scenes/game.js
@@ -3,15 +3,49 @@ import Phaser from "phaser"
 class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" })
+    this.widthTiles = 10;
+    this.heightTiles = 18;
+    this.playfield = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
   }
   preload() {
     this.load.image("field", "assets/field.png")
     this.load.audio("type-a", "assets/type-a.mp3")
+    this.load.image("blocks", "assets/debug-block-map.png")
   }
   create() {
     this.sound.stopAll()
     this.sound.play("type-a")
     this.add.image(80, 72, "field")
+
+    this.playfieldMap = this.make.tilemap({ data: this.playfield, tileWidth: 8, tileHeight: 8 })
+    const tiles = this.playfieldMap.addTilesetImage("blocks")
+    const layer = this.playfieldMap.createLayer(0, tiles, 16, 0)
+  }
+  update() {
+    let x = Math.floor(Math.random() * this.widthTiles)
+    let y = Math.floor(Math.random() * this.heightTiles)
+    let v = this.playfieldMap.getTileAt(x, y)
+    let n = (v.index + 1) % 3
+    this.playfieldMap.putTileAt(n, x, y)
   }
 }
 
